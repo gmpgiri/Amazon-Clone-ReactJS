881f2ff55199eaf6fb9d2c06a051b1c4f78855b4
Giri Madhan <gmpgiri@gmail.com>
2020-08-20 18:19:10 +0530

    working on nav bar

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div>
+            {/** logo on the left -> img */}
+            {/** Search Box */}
+            {/** 3 Links */}
+            {/** Basket Icon with a number */}
+
+        </div>
+    )
+}
+
+export default Header
