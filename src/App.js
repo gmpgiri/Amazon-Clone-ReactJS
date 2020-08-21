49b612e7b712b9355b4966e9adf6483f3738e61e
Giri Madhan <gmpgiri@gmail.com>
2020-08-21 09:27:06 +0530

    build the nav bar and header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Header from "./Header";
+import Home from "./Home";
 
 function App() {
   return (
@@ -8,13 +10,15 @@ function App() {
       <div className="App">
         <Switch>
           <Route path="/checkout">
+          <Header />
             <h1> Checkout </h1>
           </Route>
           <Route path="/login">
             <h1> Login Page</h1>
           </Route>
           <Route path="/">
-            <h1> Home Page</h1>
+            <Header />
+            <Home />
           </Route>
         </Switch>  
       </div>
---
+++ src/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Home.css";
+
+function Home() {
+    return (
+        <div className="home">
+            <img 
+            className="home__image"
+            src="https://images-eu.ssl-images-amazon.com/images/G/31/prime/Gateway/2020/May/gaming_1500x600._CB431281464_.jpg" 
+            alt="amazon header image" />
+        </div>
+    )
+}
+
+export default Home
